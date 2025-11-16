07f7e84906bc0211cf04f3b64188cfc2a35629c5
SamuelFGarcia4 <u0581789@gcould.utah.edu>
2025-11-16 16:15:54 -0700

    Fixed Routing

--- marketflow-angular/src/app/app.routes.ts
+++ marketflow-angular/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './components/home/home';
 import { EnvironmentSetup } from './components/environment-setup/environment-setup';
 import { Analysis } from './components/analysis/analysis';
 import { BotManagement } from './components/bot-management/bot-management';
+import { AnalysisDashboard } from './components/analysis-dashboard/analysis-dashboard';
 import { Login } from './components/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
 import { authGuard } from './guards/auth.guard';
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
   { path: 'environment-setup', component: EnvironmentSetup, canActivate: [authGuard] },
   { path: 'analysis', component: Analysis, canActivate: [authGuard] },
+  { path: 'analysis-dashboard', component: AnalysisDashboard, canActivate: [authGuard] },
   { path: 'bot-management', component: BotManagement, canActivate: [authGuard] },
   { path: '**', redirectTo: '/' }
 ];
