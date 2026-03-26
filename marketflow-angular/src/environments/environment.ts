1327aed2deaa5e8f129e0011dc5fc8692628627f
reignoverall <u1319222@utah.edu>
2026-03-26 12:55:02 -0600

    token

--- marketflow-angular/src/environments/environment.ts
+++ marketflow-angular/src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indxd2lqZWFkcHdiZmhhYmNuZm5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1ODIxODUsImV4cCI6MjA4NDE1ODE4NX0.v3YSC7rZ67HUkkHTFqtEWTjdXQqBu4TISsOQTguiaOg'
   },
   grokApiUrl: 'https://api.groq.com/openai/v1/chat/completions',
-  grokApiKey: '' 
+  grokApiKey: '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' 
 };
